f9ec09d02dd9681e7d35d51f388dbeeaa434cbc1
AlhelyBar <alhely.bar@gmail.com>
2021-01-17 22:28:27 -0600

    Practica 2 - Heroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr Nice', detalle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eleifend augue ut pretium sagittis. Curabitur sit amet mauris semper, blandit tortor in, mattis ex.' },
+  { id: 12, name: 'Narco', detalle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eleifend augue ut pretium sagittis. Curabitur sit amet mauris semper, blandit tortor in, mattis ex.' },
+  { id: 13, name: 'Bombasto', detalle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eleifend augue ut pretium sagittis. Curabitur sit amet mauris semper, blandit tortor in, mattis ex.' },
+  { id: 14, name: 'Celeritas', detalle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eleifend augue ut pretium sagittis. Curabitur sit amet mauris semper, blandit tortor in, mattis ex.' },
+  { id: 15, name: 'Magneta', detalle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eleifend augue ut pretium sagittis. Curabitur sit amet mauris semper, blandit tortor in, mattis ex.' },
+  { id: 16, name: 'RubberMan', detalle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eleifend augue ut pretium sagittis. Curabitur sit amet mauris semper, blandit tortor in, mattis ex.' },
+  { id: 17, name: 'Dynama', detalle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eleifend augue ut pretium sagittis. Curabitur sit amet mauris semper, blandit tortor in, mattis ex.' },
+  { id: 18, name: 'Dr IQ', detalle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eleifend augue ut pretium sagittis. Curabitur sit amet mauris semper, blandit tortor in, mattis ex.' },
+  { id: 19, name: 'Magma', detalle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eleifend augue ut pretium sagittis. Curabitur sit amet mauris semper, blandit tortor in, mattis ex.' },
+  { id: 20, name: 'Tornado', detalle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eleifend augue ut pretium sagittis. Curabitur sit amet mauris semper, blandit tortor in, mattis ex.' }
+];
